11ff81bd75c324806fa27a9134a83ba0f173a9de
DrWisWis <ibiapinoigor@gmail.com>
2025-10-30 14:35:51 -0300

    register

--- pages/auth/register.php
+++ pages/auth/register.php
@@ -3,20 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../../src/style/register.css">
+    <title>Registre-se</title>
+    <link rel="stylesheet" href="../../src/style/cadastro.css">
 </head>
 <body>
     <div class="header">
-        <p><--</p>
-        <h2>Register</h2>
+        <div class="arrow">
+            <a href="../../index.php">
+                <img src="../../src/assets/arrow-left.png" alt="">
+            </a>
+        </div>
     </div>
+        <h1>Register</h1>
+
+    <p class="personal-information">Informações Pessoais</p>
     <form method="POST" action="processaRegister.php">
         <input type="text" name="nome" placeholder="Nome completo" required>
         <input type="email" name="email" placeholder="E-mail" required>
         <input type="password" name="senha" placeholder="senha" required>
         <button type="submit" name="register" class="register">Register</button>
     </form>
-    <p>Ja tem uma conta? <a href="login.php">Login</a></p>
+    <p class="login">Ja tem uma conta? <a href="login.php">Login</a></p>
 </body>
 </html>
